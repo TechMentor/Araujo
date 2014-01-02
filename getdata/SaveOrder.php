877a2d155ff4169ceb579c7b572987230671c7ff
Owner <Owner@Madeline.att.net>
2014-01-01 19:43:21 -0800

    Order page works (95%)

--- getdata/SaveOrder.php
+++ getdata/SaveOrder.php
@@ -9,9 +9,11 @@
             // Save the new Productd data.
             $pdo = getDBConnection();
 
+            $timestamp = date('Y-m-d H:i:s');
+            
             $sqlPrepared = null;
             $sql = "INSERT INTO tblorder (OrderDate, VendorID, DueDate, RestaurantID, CreatedBy, CreatedOn) " .
-                   "VALUES(:OrderDate, :VendorID, :DueDate, :RestaurantID, :user, now())";
+                   "VALUES(:OrderDate, :VendorID, :DueDate, :RestaurantID, :user, :timestamp)";
                 //outputToText('Insert sql is ' . $sql);
             $sqlPrepared = $pdo->prepare($sql);
             $sqlPrepared->bindValue(":OrderDate", $_POST['OrderDate']);
@@ -19,7 +21,22 @@
             $sqlPrepared->bindValue(":DueDate", $_POST['DueDate']);
             $sqlPrepared->bindValue(":RestaurantID", $_POST['RestaurantID']);
             $sqlPrepared->bindValue(":user", $_SESSION['user_id']);
+            $sqlPrepared->bindValue(":timestamp", $timestamp);
             $sqlPrepared->execute();
+            
+            $sqlPrepared = null;
+            $sql = "SELECT * FROM tblorder WHERE CreatedOn LIKE :timestamp";
+                //outputToText('Insert sql is ' . $sql);
+            $sqlPrepared->bindValue(":timestamp", '%'. $timestamp .'%');
+            $sqlPrepared->execute();
+            
+            $orderid = 0;
+            
+            if($next = $sqlPrepared->fetch()) {
+		$orderid = $next['OrderID'];
+            } else {
+                throw new Exception("Error finding the order just created", '', '');
+            }
             
             for($i = 0; $i < count($_SESSION['ProductsForOrder']); $i++) {
                 $sql = "INSERT INTO tblorderproduct (OrderID, ProductID, UnitPrice, Quantity, Comment) " .
@@ -29,7 +46,7 @@
                 $sqlPrepared = null;
                 
                 $sqlPrepared = $pdo->prepare($sql);
-                $sqlPrepared->bindValue(":OrderID", 1);
+                $sqlPrepared->bindValue(":OrderID", $orderid);
                 $sqlPrepared->bindValue(":ProductID", $tProduct['ProductID']);
                 $sqlPrepared->bindValue(":UnitPrice", $tProduct['UnitPrice']);
                 $sqlPrepared->bindValue(":Quantity", $tProduct['Quantity']);
